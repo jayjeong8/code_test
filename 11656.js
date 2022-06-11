73f97d6d4b68156802b2e971705f83a82c13329c
jayjeong8 <zuni4242@gmail.com>
2022-06-11 15:57:44 +0900

    기초1-자료구조1(참고)-접미사 배열

--- 11656.js
+++ 11656.js
@@ -12,3 +12,11 @@ for (let i = 0, n = input.length; i < n; i++){
 }
 console.log(dictionary.sort().join('\n'));
 
+//sort는 복사본이 아닌 원배열을 정렬시킨다.
+// 복사본을 만들때 바로 대입하는 대신 스프레드 등의 형식을 이용하여야,
+// 복사본.sort시 원본 배열도 바뀌는 것을 방지할 수 있다.
+/*
+const ans = [...dictionary];
+
+console.log(ans.sort());
+console.log(dictionary);*/
