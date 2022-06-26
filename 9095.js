8b8004e72ffc8d9f10c5cb937fb787653f3a3112
jayjeong8 <zuni4242@gmail.com>
2022-06-26 22:27:42 +0900

    기초2-브루트포스(재귀)-1,2,3 더하기

--- 9095.js
+++ 9095.js
@@ -2,9 +2,36 @@
 //1과 2와 3으로 N을 구성할 수 있는 방법의 수
 //N은 11보다 작다
 
-const input = require('fs').readFileSync('/dev/stdin','utf8').trim().split('\n').map(Number);
-
+const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
 const ans = [];
+
+//풀이2 - 브루트포스 재귀 방법
+//count가 N이 되면 ans에 방법의 수 구한 것을 넣기.
+let sum = 0;
+
+for (let i = 1; i <= input[0]; i++) {
+    sum = 0;
+    bf(0, input[i]);
+    ans.push(sum);
+}
+console.log(ans.join('\n'));
+
+function bf(count, n) {
+    if(count > n){
+        return; //안멈춰주면 무한루프
+    }
+    if (count === n) {
+        sum++;
+        return;
+    }
+    for (let i = 1; i <= 3; i++) {
+        bf(count + i, n);
+    }
+}
+
+
+/* 풀이1 - 다이나믹프로그래밍 방법
+
 const nMemo = {
     1: 1,
     2: 2,
@@ -22,3 +49,4 @@ for (let i=1; i<= input[0]; i++){
 }
 
 console.log(ans.join('\n'));
+*/
